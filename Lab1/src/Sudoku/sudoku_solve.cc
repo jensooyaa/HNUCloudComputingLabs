30e3c699b2962d260706e0ad9c44793e8bfd0ea8
dongliushi <2445264666@qq.com>
2022-03-09 11:06:16 +0800

    修改了cout输出'\n'

--- Lab1/src/Sudoku/sudoku_solve.cc
+++ Lab1/src/Sudoku/sudoku_solve.cc
@@ -14,15 +14,20 @@ const int thread_max_num = 100;
 thread th[thread_max_num];
 char puzzle[128];
 
-int thread_num = 1;
+int thread_num;
 int puzzle_buf[puzzle_num][N];
 
-void solve(int start, int end) {
+void solve(const int start, const int end) {
   for (int i = start; i < end; i++)
     solve_sudoku_dancing_links(puzzle_buf[i]);
 }
-void init() { thread_num = thread::hardware_concurrency(); }
-void trans(char in[N], int *target) {
+
+void init() {
+  ios_base::sync_with_stdio(false);
+  thread_num = thread::hardware_concurrency();
+}
+
+void trans(const char in[N], int *target) {
   for (int i = 0; i < N; i++) {
     target[i] = in[i] - 48;
   }
@@ -39,17 +44,15 @@ void file_handle(string file_name) {
         break;
       }
       input_file.getline(puzzle, N + 1);
-      if (strlen(puzzle) >= N) {
-        trans(puzzle, puzzle_buf[line_count]);
-        line_count++;
-      }
+      if (strlen(puzzle) >= N)
+        trans(puzzle, puzzle_buf[line_count++]);
     };
 
     // 将数据分块给线程处理
-    int start = 0, step = (line_count + 1 + thread_num) / thread_num;
-    for (int i = 0; i < thread_num; i++, start += step) {
+    int start = 0, len = (line_count + thread_num) / thread_num;
+    for (int i = 0; i < thread_num; i++, start += len) {
       th[i] = thread(solve, start,
-                     (start + step >= line_count) ? line_count : start + step);
+                     (start + len >= line_count) ? line_count : start + len);
     }
     for (int i = 0; i < thread_num; i++)
       th[i].join();
@@ -58,12 +61,12 @@ void file_handle(string file_name) {
     for (int i = 0; i < line_count; i++) {
       for (int j = 0; j < N; j++)
         cout << puzzle_buf[i][j];
-      cout << endl;
+      cout << "\n";
     }
   }
   input_file.close();
 }
-int main(int argc, char *argv[]) {
+int main() {
   init();
   string file_name;
   while (getline(cin, file_name)) {
